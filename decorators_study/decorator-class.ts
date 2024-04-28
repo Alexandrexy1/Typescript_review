4d9e62ff8d32df275d1082edc142217382653cb2
Alexandre Nascimento da Silva <Alexandrenas2003@gmail.com>
2024-04-28 11:57:24 -0300

    decorator factories

--- decorators_study/decorator-class.ts
+++ decorators_study/decorator-class.ts
@@ -1,4 +1,4 @@
-@decorator
+@decorator("Anything", "else")
 export class Account {
     constructor(
         public name: string,
@@ -7,14 +7,21 @@ export class Account {
     ) {}
 }
 
-function decorator(target: any): any {
-    return class extends target {
-        name: string;
-        constructor(...args: any[]) {
-            super(...args);
-            this.name = args[0].split("").reverse().join("");
-        }
-    };
+function decorator(param: string, param2: string) {
+    return function decorator(target: any): any {
+        return class extends target {
+            name: string;
+
+            constructor(...args: any[]) {
+                super(...args);
+                this.name = `${this.invertString(args[0])} ${param} ${param2}` ;
+            }
+
+            invertString(value: string) {
+                return value.split("").reverse().join("");
+            }
+        };
+    }
 }
 
 const account = new Account("Alex", "0001", 200);
